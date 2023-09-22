4525add1780734980f23ed48c4a3bb51a4c46a03
jayakumar03 <jaimech61@gmail.com>
2023-09-22 12:06:38 +0530

    Added state in homepage to blur main image

--- src/components/Homepage/Groups.jsx
+++ src/components/Homepage/Groups.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 export const Groups = ({ groupDetails }) => {
+  const [openNoteModal, setNoteModal] = useState(false);
+
   return (
     <>
       {groupDetails.map(({ groupName, color }) => {
         return (
-          
-
-          
           <div className="group-container">
-            <button>
-            <span className="group-logo" style={{ backgroundColor: color }}>
-              <p>{groupName.slice(0, 2).toUpperCase()}</p>
-            </span>
-            <p className="group-name">{groupName}</p>
+            <button
+              onClick={() => {
+                setNoteModal(true);
+              }}
+            >
+              <span className="group-logo" style={{ backgroundColor: color }}>
+                <p>{groupName.slice(0, 2).toUpperCase()}</p>
+              </span>
+              <p className="group-name">{groupName}</p>
             </button>
+            {openNoteModal ? <IndiviualNotes /> : ""}
           </div>
         );
       })}
--- src/components/Homepage/NotesGroup.jsx
+++ src/components/Homepage/NotesGroup.jsx
@@ -3,18 +3,24 @@ import { useState } from "react";
 import { Modal } from "./Modal.jsx";
 import { Groups } from "./Groups";
 
-export const NotesGroup = () => {
+export const NotesGroup = ({ setShowImage }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [groupDetails, setGroupDetails] = useState([]);
 
   function openModal(e) {
     e.preventDefault();
+    setShowImage(true);
     setModalOpen(true);
   }
 
   return (
     <>
       <div className="container">
+        <h1>Pocket Notes</h1>
+        <button onClick={openModal}>Create Notes Group</button>
+
+        <Groups groupDetails={groupDetails} />
+
         {modalOpen && (
           <Modal
             closeModal={() => setModalOpen(false)}
@@ -22,12 +28,6 @@ export const NotesGroup = () => {
             groupDetails={groupDetails}
           />
         )}
-
-        
-        <h1>Pocket Notes</h1>
-        <button onClick={openModal}>Create Notes Group</button>
-
-        <Groups groupDetails={groupDetails} />
       </div>
     </>
   );
