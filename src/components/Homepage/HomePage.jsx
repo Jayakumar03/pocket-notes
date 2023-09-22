73e70cd5c03d96c221a67164d2c66ffbf7708370
jayakumar03 <jaimech61@gmail.com>
2023-09-22 12:04:23 +0530

    Create main image for homapage

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
+import { MainImage } from "./MainImage";
 import { NotesGroup } from "./NotesGroup";
 
-
 export const HomePage = () => {
-    return(
-        <>
-        <NotesGroup/>
-        
-        
-        </>
-    )
-    
-}
+    const [showImage, setShowImage] = useState(false)
+
+  return (
+    <>
+      <NotesGroup setShowImage={setShowImage} />
+      {!showImage ?  <MainImage /> : ""  }
+      
+    </>
+  );
+};
---
+++ src/components/Homepage/MainImage.jsx
@@ -0,0 +1,23 @@
+import "./mainImage.css";
+
+export const MainImage = () => {
+  return (
+    <>
+      <div className="main-container">
+        <div className="main-wrapper">
+          <div className="main-image-container">
+            <img
+              src="/image-removebg-preview 1.png"
+              alt="main backGround image"
+            />
+          </div>
+          <div className="main-heading-container">
+            <h2>Pocket Notes</h2>
+            <p>Send and receive messages without keeping your phone online. </p>
+            <p>UsePocket Notes on up to 4 linked devices and 1 mobile phone</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
